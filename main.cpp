282f2cc0115b33e15dd2fad90b8822f76471afc7
Rey Almaraz <ChrisRey@wlan-t-195-63-dhcp.CSUChico.EDU>
2018-02-13 11:13:50 -0800

    Inputed cout and cin statements.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors:Chris Rey Almaraz
 #include<iostream>
 #include<string>
 
@@ -7,9 +7,14 @@ using namespace std;
 int main()
 {
   //declare variables
-
+ string firstname;
+ string lastname;
   //get user input
+ cout<<"Whats your firstname?\n";
+ cin>>firstname;
 
+ cout<<"What is your lastname?\n";
+ cin>>lastname;
   //tell fortune
 
   return 0;

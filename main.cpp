452054ed71671e22f8d67007689cffe8a619cc7d
Rey Almaraz <ChrisRey@wlan-t-195-63-dhcp.CSUChico.EDU>
2018-02-15 11:48:11 -0800

    Gave them their fortune.

--- main.cpp
+++ main.cpp
@@ -10,16 +10,37 @@ int main()
  string firstname;
  string lastname;
  int lucky;
+char firstint;
+char lastint;
   //get user input
  cout<<"Whats your firstname?\n";
  cin>>firstname;
+firstint= firstname[0];
 
  cout<<"What is your lastname?\n";
  cin>>lastname;
   //tell fortune
 lucky= firstname.length();
+
 cout<<"Welcome, "<<firstname[0]<<"."<<lastname[0]<<"., here is your fortune...\n";
 cout<<" your lucky number is "<<lucky<< endl;
 
-  return 0;
+if (firstint=='a'||firstint=='e'||firstint=='i'||firstint=='o'||firstint=='u'||firstint=='A'||firstint=='E'||firstint=='I'||firstint=='O'||firstint=='U')
+{	cout<<"you are destined to be famous.\n";
+}
+else 
+{
+       cout<<	"you should keep a low profile.\n";
+}
+int len=lastname.length();
+
+ lastint = lastname[len-1];
+
+
+if (lastint=='a'||lastint=='e'||lastint=='i'||lastint=='o'||lastint=='u'||lastint=='A'||lastint=='E'||lastint=='I'||lastint=='O'||lastint=='U')
+{ cout<<"you have already met your true love.\n";}
+
+cout<<"have a good day!\n";
+
+return 0;
 }
